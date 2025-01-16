1b49380b311476416edb5061ad11fd1f810fd7a5
Hayes <goteevreslajd@gmail.com>
2025-01-16 18:14:30 +0200

    Add files via upload

---
+++ seval.hpp
@@ -0,0 +1,168 @@
+/* seval.hpp */
+#pragma once
+
+#include <iostream>
+#include <type_traits>
+#include <cmath>
+
+#if !defined(SEVAL_INLINE)
+#define SEVAL_INLINE inline
+#endif // SEVAL_INLINE
+
+
+namespace seval {
+enum Sign : int8_t {
+    SIGN_NEGATIVE = -1,
+    SIGN_POSITIVE =  1,
+    SIGN_NONE     =  0,
+};
+
+SEVAL_INLINE void skip_(size_t& i, size_t cnt) {
+    i += cnt;
+}
+
+SEVAL_INLINE void next_(size_t& i) {
+    skip_(i, 1);
+}
+
+template <typename StrT>
+Sign get_sign(StrT str, size_t& i) {
+    if (str[i] == '-') {
+        return SIGN_NEGATIVE;
+    } else if (str[i] == '+') {
+        return SIGN_POSITIVE;
+    } else {
+        return SIGN_NONE;
+    }
+}
+
+SEVAL_INLINE bool is_decimal_ch(const char literal) {
+    return (literal >= '0' && literal <= '9');
+}
+
+SEVAL_INLINE bool is_lower_hex_ch(const char literal) {
+    return (literal >= 'a' && literal <= 'f');
+}
+
+SEVAL_INLINE bool is_upper_hex_ch(const char literal) {
+    return (literal >= 'A' && literal <= 'F');
+}
+
+SEVAL_INLINE bool is_hexadecimal_ch(const char literal) {
+    return (is_decimal_ch(literal) || is_lower_hex_ch(literal) || is_upper_hex_ch(literal));
+}
+
+template <typename T>
+SEVAL_INLINE T evaluate_decimal_ch(const char ch) {
+    // if (!is_decimal_ch(ch)) return 0;
+    return (ch - '0');
+}
+
+template <typename T>
+SEVAL_INLINE T evaluate_lower_hex_ch(const char ch) {
+    return (ch - 'a' + 10);
+}
+
+template <typename T>
+SEVAL_INLINE T evaluate_upper_hex_ch(const char ch) {
+    return (ch - 'A' + 10);
+}
+
+template <typename T>
+SEVAL_INLINE T evaluate_hexadecimal_ch(const char ch) {
+    if (is_decimal_ch(ch)) return evaluate_decimal_ch<T>(ch);
+    if (is_lower_hex_ch(ch)) return evaluate_lower_hex_ch<T>(ch);
+    if (is_upper_hex_ch(ch)) return evaluate_upper_hex_ch<T>(ch);
+    return 0;
+}
+
+template <typename T, typename StrT>
+SEVAL_INLINE void evaluate_decimal_literal(StrT str, T& number, size_t& i) {
+    while (str[i] != '\0' && is_decimal_ch(str[i])) {
+        number = number * 10 + evaluate_decimal_ch<T>(str[i]);
+        next_(i);
+    }
+}
+
+template <typename T, typename StrT>
+SEVAL_INLINE void evaluate_floatpoint_literal(StrT str, T& number, size_t& i, T decimalPlace) {
+    while (str[i] != '\0' && is_decimal_ch(str[i])) {
+        number += evaluate_decimal_ch<T>(str[i]) * decimalPlace;
+        decimalPlace /= static_cast<T>(10);
+        next_(i);
+    }
+}
+
+template <typename T, typename StrT>
+SEVAL_INLINE void evaluate_exponent_literal(StrT str, T& number, size_t& i) {
+    if (str[i] == 'e' || str[i] == 'E') {
+        next_(i);
+        Sign expSign = SIGN_POSITIVE;
+
+        if (str[i] == '-') {
+            expSign = SIGN_NEGATIVE;
+            next_(i);
+        } else if (str[i] == '+') {
+            next_(i);
+        }
+
+        int exponent = 0;
+        while (str[i] != '\0' && is_decimal_ch(str[i])) {
+            exponent = exponent * 10 + evaluate_decimal_ch<int>(str[i]);
+            next_(i);
+        }
+        number *= std::pow(static_cast<T>(10), static_cast<T>(expSign * exponent));
+    }
+}
+
+template <typename T, typename StrT>
+SEVAL_INLINE void evaluate_hexadecimal_literal(StrT str, T& number, size_t& i) {
+    while (str[i] != '\0' && is_hexadecimal_ch(str[i])) {
+        number = number * 16 + evaluate_hexadecimal_ch<T>(str[i]);
+        i++;
+    }
+}
+
+template <typename StrT>
+SEVAL_INLINE bool has_hexadecimal_prefix(StrT literal, size_t& i) {
+    return (literal[i] == '0') && (literal[i + 1] == 'x' || literal[i + 1] == 'X');
+}
+
+template <typename T, typename StrT>
+SEVAL_INLINE T evaluate(StrT str, bool consideSign = true, bool consideFloatPoint = true, bool consideHex = true, bool consideExponent = true) {
+    static_assert(std::is_arithmetic<T>::value, "Template parameter T must be an arithmetic type (integral or floating-point).");
+    
+    T number = 0;
+    size_t i = 0;
+
+    Sign sign = SIGN_POSITIVE;
+
+    if (consideSign) {
+        Sign intermediateSign = get_sign<StrT>(str, i);
+        if (intermediateSign != SIGN_NONE) {
+            sign = intermediateSign;
+            skip_(i, 1); /* Eat: sigSym (+ or -) */
+        }
+    }
+
+    if (consideHex && has_hexadecimal_prefix<StrT>(str, i)) {
+        skip_(i, 2); /* Eat: hexadecimalPrefix (0x or 0X) */
+        evaluate_hexadecimal_literal<T, StrT>(str, number, i);
+    }
+    else {
+        evaluate_decimal_literal<T, StrT>(str, number, i);
+    }
+
+    if (std::is_floating_point<T>::value && consideFloatPoint && str[i] == '.') {
+        i++;
+        T decimalPlace = static_cast<T>(0.1);
+        evaluate_floatpoint_literal<T, StrT>(str, number, i, decimalPlace);
+    }
+
+    if (std::is_floating_point<T>::value && consideFloatPoint && consideExponent) {
+        evaluate_exponent_literal<T, StrT>(str, number, i);
+    }
+
+    return consideSign ? number * sign : number;
+}
+} /* seval */
---
+++ test.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cassert>
+#include "seval.hpp"
+
+template <typename T>
+bool floatpoint_compare(T f1, T f2, T deviation = 1e-6) {
+    return (f1 - f2) <= deviation && (f2 - f1) <= deviation;
+}
+
+void seval_test() {
+    /* 8 BIT */
+    {
+        assert((seval::evaluate<int8_t, const char*>("127")) == 127);  /* 127 because int8_t is signed number */
+        assert((seval::evaluate<uint8_t, const char*>("255")) == 255);
+        assert((seval::evaluate<int8_t, const char*>("-128")) == -128);
+        assert((seval::evaluate<uint8_t, const char*>("0")) == 0);
+    }
+
+    /* 16 BIT */
+    {
+        assert((seval::evaluate<int16_t, const char*>("32767")) == 32767);
+        assert((seval::evaluate<uint16_t, const char*>("65535")) == 65535);
+        assert((seval::evaluate<int16_t, const char*>("-32768")) == -32768);
+        assert((seval::evaluate<uint16_t, const char*>("0")) == 0);
+    }
+
+    /* 32 BIT */
+    {
+        assert((seval::evaluate<int32_t, const char*>("2147483647")) == 2147483647);
+        assert((seval::evaluate<uint32_t, const char*>("4294967295")) == 4294967295U);
+        assert((seval::evaluate<int32_t, const char*>("-2147483648")) == -2147483648);
+        assert((seval::evaluate<uint32_t, const char*>("0")) == 0);
+    }
+
+    /* 64 BIT */
+    {
+        assert((seval::evaluate<int64_t, const char*>("9223372036854775807")) == 9223372036854775807LL);
+        assert((seval::evaluate<uint64_t, const char*>("18446744073709551615")) == 18446744073709551615ULL);
+        assert((seval::evaluate<int64_t, const char*>("-9223372036854775808")) == -9223372036854775807LL - 1);
+        assert((seval::evaluate<uint64_t, const char*>("0")) == 0);
+    }
+    /* HEXADECIMAL */
+    {
+        assert((seval::evaluate<int, const char*>("123") == 123)); /* without prefix */
+        assert((seval::evaluate<int, const char*>("0x123") == 0x123)); /* with prefix */
+
+        assert((seval::evaluate<int, const char*>("-123") == -123)); /* without prefix */
+        assert((seval::evaluate<int, const char*>("-0x123") == -0x123)); /* with prefix */
+
+        assert((seval::evaluate<int, const char*>("0xA3F") == 0xA3F));
+        assert((seval::evaluate<int, const char*>("0x1aF") == 0x1AF));
+    }
+    /* FLOAT POINT */
+    {
+        assert(floatpoint_compare(seval::evaluate<float, const char*>("3.14"), 3.14f));
+        assert(floatpoint_compare(seval::evaluate<float, const char*>("3.14"), 3.14f));
+        assert(floatpoint_compare(seval::evaluate<float, const char*>("-3.14"), -3.14f));
+        assert(floatpoint_compare(seval::evaluate<double, const char*>("2.718281828459045"), 2.718281828459045));
+        assert(floatpoint_compare(seval::evaluate<double, const char*>("-2.718281828459045"), -2.718281828459045));
+        assert(floatpoint_compare(seval::evaluate<long double, const char*>("1.618033988749895"), 1.618033988749895L));
+        assert(floatpoint_compare(seval::evaluate<long double, const char*>("-1.618033988749895"), -1.618033988749895L));
+    }
+    /* FLOAT POINT WITH EXPONENT */
+    {
+        assert(floatpoint_compare(seval::evaluate<float, const char*>("3.14e2"), 314.0f));
+        assert(floatpoint_compare(seval::evaluate<float, const char*>("-3.14E-2"), -0.0314f));
+    }
+}
+
+int main() {
+    seval_test();
+    std::cout << "All tests passed!" << std::endl;
+    return 0;
+}
